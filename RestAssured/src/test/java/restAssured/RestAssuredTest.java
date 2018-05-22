16e6dc8d29250258023bf95db26d250468b1e830
Hema <Hema@Hema-PC>
2018-05-22 08:27:47 +0530

    Initial Commit

---
+++ RestAssured/src/test/java/restAssured/RestAssuredTest.java
@@ -0,0 +1,23 @@
+package restAssured;
+
+import org.testng.annotations.Test;
+
+public class RestAssuredTest {
+	
+	
+	@Test
+	public void restTest() {
+		System.out.println("Rest Assured Test Case");
+		
+	}
+	@Test
+	public void restTest1() {
+		System.out.println("Rest Assured Test Case1");
+		
+	}
+	@Test
+	public void restTest2() {
+		System.out.println("Rest Assured Test Case2");
+		
+	}
+}
